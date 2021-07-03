dc7ee95462249b7350cce57c87e563f6955866ec
Sujata-123 <sujata672singh@gmail.com>
2021-07-04 02:56:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-aigbpt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-aigbpt)
---
+++ script.js
@@ -0,0 +1,35 @@
+// function signUp() {
+//   let name = document.getElementById('name').value;
+//   let age = document.getElementById('age').value;
+//   let email = document.getElementById('email').value;
+//   let pass = document.getElementById('pass').value;
+
+//   // localStorage.setItem('name1', name);
+//   // localStorage.setItem('age1', age);
+//   // localStorage.setItem('email1t', email);
+//   // localStorage.setItem('pass1', pass);
+
+//   contactDetail = {
+//     name: name,
+//     age: age,
+//     email: email,
+//     pass: pass
+//   };
+//   localStorage.setItem('contactDetail', JSON.stringify(contactDetail));
+// }
+/* <script> 
+function login() {
+  // window.location = 'home.html';
+  console.log('Inside Login');
+  let email = document.getElementById('email').value;
+  let pass = document.getElementById('pass').value;
+
+  let email = localStorage.getItem('email');
+  let pass = localStorage.getItem('pass');
+  if (email === email && pass === pass) {
+    window.location = 'home.html';
+  } else {
+    alert('Email and pass is incorrect');
+  }
+}
+ </script> */
